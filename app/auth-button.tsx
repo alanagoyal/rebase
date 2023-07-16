eea7015fe11c80f8cfc2d2379e10696589a3ea3b
alanagoyal <alanaan@gmail.com>
2023-07-16 13:18:52 -0700

    Saving non-supabase stuff

--- app/auth-button.tsx
+++ app/auth-button.tsx
@@ -9,6 +9,7 @@ export default function AuthButton() {
   const supabase = createClientComponentClient();
 
   async function signInWithGoogle() {
+    console.log("in auth button");
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
