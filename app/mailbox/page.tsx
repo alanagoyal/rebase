3c2dce50aa8261d969e033e2bdf9c37c64ff709c
mravigulan <mravigulan@netflix.com>
2023-08-18 21:02:48 -0700

    got baseline email form working

--- app/mailbox/page.tsx
+++ app/mailbox/page.tsx
@@ -1,16 +1,194 @@
+"use client"; // This is a client component 👈🏽
+
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import Tiptap from "@/components/tiptap";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+
+import { Controller, useForm } from "react-hook-form";
+
+import * as z from "zod";
 
 export default function Mailbox() {
+  const [isTiptapOpen, setIsTiptapOpen] = useState(false);
+
+  console.log(isTiptapOpen);
+
+  const handleNewMessageClick = () => {
+    setIsTiptapOpen(true);
+  };
+
+  const emailFormSchema = z.object({
+    // to: z
+    //   .array(
+    //     z.string().email({ message: "Please enter a valid email address." })
+    //   )
+    //   .min(1, "Please enter at least one recipient."),
+    // to: z
+    //   .string()
+    //   .email({ message: "YAM YAM YAM gimme valid email at least mister." }),
+    to: z.string().refine(
+      (value) => {
+        const emails = value.split(",").map((email) => email.trim());
+        return emails.every(
+          (email) => z.string().email().safeParse(email).success
+        );
+      },
+      {
+        message: "Please enter valid email addresses separated by commas.",
+      }
+    ),
+    subject: z.string().optional(),
+    body: z.string().optional(),
+    cc: z
+      .array(
+        z.string().email({ message: "Please enter a valid email address." })
+      )
+      .optional(),
+    bcc: z
+      .array(
+        z.string().email({ message: "Please enter a valid email address." })
+      )
+      .optional(),
+    attachments: z.array(z.string()).optional(),
+  });
+
+  type EmailFormValues = z.infer<typeof emailFormSchema>;
+
+  const form = useForm<EmailFormValues>({
+    resolver: zodResolver(emailFormSchema),
+    defaultValues: {
+      to: "",
+      subject: "",
+      body: "",
+      cc: [],
+      bcc: [],
+      attachments: [],
+    },
+  });
+
+  async function onSubmit(data: EmailFormValues) {
+    console.log("submitting", data);
+    try {
+      const updates = {
+        to: [data.to],
+        subject: "",
+        body: data.body,
+        cc: [],
+        bcc: [],
+        attachments: [],
+      };
+      console.log("check whether u need to update hardocded  values");
+      console.log(updates);
+
+      //   let { error } = await supabase.from("members").insert(updates);
+      //   if (error) throw error;
+      //   toast({
+      //     description: "Your member has been added",
+      //   });
+      //   router.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className="flex-1 space-y-4 p-8 pt-6">
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">Mailbox</h2>
-        <div className="flex items-center space-x-2">
-          <Button>New Message</Button>
-        </div>
+        <div className="flex items-center space-x-2"></div>
       </div>
-      <Tiptap />
+      <Dialog open={isTiptapOpen}>
+        <DialogTrigger asChild>
+          <Button onClick={handleNewMessageClick}>New Message</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-xl">
+          <DialogHeader>
+            <DialogTitle>Compose Email</DialogTitle>
+            <div className="grid gap-4 py-4">
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-8"
+                >
+                  {" "}
+                  <FormField
+                    control={form.control}
+                    name="to"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+                        <div className="space-y-0.5">
+                          <FormLabel className="text-base mx-2">To</FormLabel>
+                        </div>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="subject"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+                        <div className="space-y-0.5">
+                          <FormLabel className="text-base mx-2">
+                            Subject
+                          </FormLabel>
+                        </div>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <Controller
+                    name="body"
+                    control={form.control}
+                    render={({ field }) => (
+                      <>
+                        <Tiptap field={field} setValue={form.setValue} />
+                      </>
+                    )}
+                  />
+                  <div className="py-1 flex justify-center">
+                    <Button
+                      type="submit"
+                      onClick={() => {
+                        console.log("clicked send");
+                        // setIsTiptapOpen(false);
+                      }}
+                      className="bg-[#9FACE6] text-white font-bold py-2 px-4 rounded w-full"
+                    >
+                      Send
+                    </Button>
+                  </div>
+                </form>
+              </Form>
+            </div>
+
+            {/* <DialogDescription>
+              Please enter the first name, last name, and email
+            </DialogDescription> */}
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
--- components/tiptap.js
+++ components/tiptap.js
@@ -1,15 +1,185 @@
-"use client";
+import "./styles.css";
 
-import { useEditor, EditorContent } from "@tiptap/react";
+import Highlight from "@tiptap/extension-highlight";
+import TextAlign from "@tiptap/extension-text-align";
+import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import React from "react";
+import Image from "@tiptap/extension-image";
 
-const Tiptap = () => {
-  const editor = useEditor({
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
-  });
+import { Controller, useForm } from "react-hook-form";
 
-  return <EditorContent editor={editor} />;
+const MenuBar = ({ editor }) => {
+  if (!editor) {
+    return null;
+  }
+
+  const buttonBaseStyles = "px-2 py-1 rounded border border-gray-300";
+  const activeButtonStyles = "bg-primary text-white";
+  const inactiveButtonStyles = "bg-secondary text-gray-700";
+  const addImage = () => {
+    const url = window.prompt("URL");
+
+    if (url) {
+      editor.chain().focus().setImage({ src: url }).run();
+    }
+  };
+
+  return (
+    <>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        className={`${buttonBaseStyles} ${
+          editor.isActive("heading", { level: 1 })
+            ? activeButtonStyles
+            : inactiveButtonStyles
+        }`}
+      >
+        h1
+      </button>
+
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        className={`${buttonBaseStyles} ${
+          editor.isActive("heading", { level: 2 })
+            ? activeButtonStyles
+            : inactiveButtonStyles
+        }`}
+      >
+        h2
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        className={`${buttonBaseStyles} ${
+          editor.isActive("heading", { level: 3 })
+            ? activeButtonStyles
+            : inactiveButtonStyles
+        }`}
+      >
+        h3
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setParagraph().run()}
+        className={`${buttonBaseStyles} ${
+          editor.isActive("paragraph")
+            ? activeButtonStyles
+            : inactiveButtonStyles
+        }`}
+      >
+        paragraph
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        className={`${buttonBaseStyles} ${
+          editor.isActive("bold") ? activeButtonStyles : inactiveButtonStyles
+        }`}
+      >
+        bold
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+        className={`${buttonBaseStyles} ${
+          editor.isActive("italic") ? activeButtonStyles : inactiveButtonStyles
+        }`}
+      >
+        italic
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        className={`${buttonBaseStyles} ${
+          editor.isActive("strike") ? activeButtonStyles : inactiveButtonStyles
+        }`}
+      >
+        strike
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHighlight().run()}
+        className={`${buttonBaseStyles} ${
+          editor.isActive("highlight")
+            ? activeButtonStyles
+            : inactiveButtonStyles
+        }`}
+      >
+        highlight
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign("left").run()}
+        className={`${buttonBaseStyles} ${
+          editor.isActive({ textAlign: "left" })
+            ? activeButtonStyles
+            : inactiveButtonStyles
+        }`}
+      >
+        left
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign("center").run()}
+        className={`${buttonBaseStyles} ${
+          editor.isActive({ textAlign: "center" })
+            ? activeButtonStyles
+            : inactiveButtonStyles
+        }`}
+      >
+        center
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign("right").run()}
+        className={`${buttonBaseStyles} ${
+          editor.isActive({ textAlign: "right" })
+            ? activeButtonStyles
+            : inactiveButtonStyles
+        }`}
+      >
+        right
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign("justify").run()}
+        className={`${buttonBaseStyles} ${
+          editor.isActive({ textAlign: "justify" })
+            ? activeButtonStyles
+            : inactiveButtonStyles
+        }`}
+      >
+        justify
+      </button>
+      <button
+        className={`${(buttonBaseStyles, inactiveButtonStyles)} `}
+        onClick={addImage}
+      >
+        add image from URL
+      </button>
+    </>
+  );
 };
 
-export default Tiptap;
+export default ({ setValue, field }) => {
+  const editor = useEditor({
+    onUpdate: ({ editor }) => {
+      console.log("updating to", editor.getHTML());
+      // Update the form field value when the editor's content changes
+      field.onChange(editor.getHTML());
+    },
+    onMounted: () => {
+      // Set the initial value when the editor is mounted
+      setValue(fieldName, editor.getHTML());
+      console.log("mounte");
+    },
+    extensions: [
+      StarterKit,
+      Image,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+      Highlight,
+    ],
+    content: ``,
+  });
+
+  return (
+    <div>
+      <MenuBar editor={editor} />
+      {console.log(editor?.getHTML())}
+      <EditorContent editor={editor} />
+    </div>
+  );
+};
