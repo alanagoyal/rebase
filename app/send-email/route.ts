9b2d1ce1e43dde26495d1c4a38244b5e3a0df03b
alanagoyal <alanaan@gmail.com>
2023-09-05 10:56:52 -0700

    fix typescrip error in send-email

--- app/send-email/route.ts
+++ app/send-email/route.ts
@@ -3,18 +3,26 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
+interface CreateEmailResponse {
+  statusCode: number;
+  message: string;
+}
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+
 export async function POST(req: Request, res: NextResponse) {
-    const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
-    const body = await req.json()
-    console.log(body)
+  try {
+    const body = await req.json();
     const payload = {
-        from: body.from_email,
-        to: body.to_emails,
-        subject: body.subject,
-        html: body.body,
-      }
-      console.log(payload)
+      from: body.from_email,
+      to: body.to_emails,
+      subject: body.subject,
+      html: body.body,
+    };
+
     const emailData = await resend.emails.send(payload);
 
-    return NextResponse.json({ success: emailData?.statusCode < 300, result: emailData, payload: payload }, { status: emailData?.statusCode, statusText: emailData.message })
+    return NextResponse.json(emailData);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
 }
